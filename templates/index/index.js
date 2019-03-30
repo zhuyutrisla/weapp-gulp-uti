13f3e31315f8424613c6f77af233fad2de21d68b
zhuyu <444531326@qq.com>
2019-03-30 11:17:29 +0800

    生成pages 模板

---
+++ templates/index/index.js
@@ -0,0 +1,34 @@
+// index.js
+const { api } = require('../../util/api')
+
+Page({
+    data: {
+    },
+    onLoad(options) {
+      // Do some initialize when page load.
+    },
+    onReady() {
+      // Do something when page ready.
+    },
+    onShow() {
+      // Do something when page show.
+    },
+    onHide() {
+      // Do something when page hide.
+    },
+    onUnload() {
+      // Do something when page close.
+    },
+    onPullDownRefresh() {
+      // Do something when pull down.
+    },
+    onReachBottom() {
+      // Do something when page reach bottom.
+    },
+    onShareAppMessage() {
+      // return custom share data when user share.
+    },
+    onPageScroll() {
+      // Do something when page scroll
+    },
+  })
