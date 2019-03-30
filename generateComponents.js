11890360cf9547d88b30dc4c91726b14aac442a5
zhuyu <444531326@qq.com>
2019-03-30 11:40:00 +0800

    生成component 模板

---
+++ generateComponents.js
@@ -0,0 +1,78 @@
+/**
+ * 生成 components 的文件  
+ * node generateCompnents test
+ */
+
+let fs = require('fs');
+let basepath = './templates/component';
+let srcPath = './src/components/'
+let cptName = process.argv.splice(2)[0];
+if (!cptName) {
+  console.log('请传入参数')
+  return 
+}
+let path = cptName.split('/');
+let name = path[path.length - 1];
+let writes = [`index.js`, `index.wxml`, `index.less`, `index.json`];
+let reads = [`${basepath}/index.js`, `${basepath}/index.wxml`, `${basepath}/index.less`, `${basepath}/index.json`];
+let file = [];
+
+//检测是否存在文件夹
+let exists = function () {
+  return new Promise((res, rej) => {
+    (async function () {
+      if (fs.existsSync(srcPath + name)){
+        rej(`${name}: 当前文件夹已存在`)
+      } else {
+        let src = await mkdir(name)
+        res(src)
+      }
+    })()
+  })
+}
+//建立文件夹
+let mkdir = function (a) {
+  return new Promise((res, rej) => {
+    fs.mkdir(srcPath + a, (err) => {
+      if (err) rej(err);
+      res(srcPath);
+    });
+  })
+}
+//读取模板文件内容，并替换为目标组件
+let readFile = function () {
+  return new Promise((res) => {
+    for (let a of reads) {
+      let text = fs.readFileSync(a).toString();
+      file.push(text)
+    }
+    res(file);
+  })
+}
+//生成文件，并填入之前读取的文件内容
+let writeFile = function (file) {
+  return new Promise((res, rej) => {
+    (async function () {
+      for (let a of writes) {
+        console.log(`${srcPath + name}/${a}`)
+        await fs.writeFile(`${srcPath + name}/${a}`,
+        file[writes.indexOf(a)], 
+        (err) => {
+            if (err) rej(err)
+        })
+      }
+      res('succ');
+    })()
+  })
+}
+async function creatCpt() {
+  try {
+    let src = await exists();
+    await writeFile(await readFile());
+    return console.log(`${name} component 已生成`)
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
+creatCpt();
---
+++ templates/component/index.js
@@ -0,0 +1,41 @@
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  behaviors: [],
+
+  properties: {
+    myProperty: { // 属性名
+      type: String, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
+      value: '', // 属性初始值（可选），如果未指定则会根据类型选择一个
+      observer(newVal, oldVal, changedPath) {
+        // 属性被改变时执行的函数（可选），通常 newVal 就是新设置的数据， oldVal 是旧数
+        // 新版本基础库不推荐使用这个字段，而是使用 Component 构造器的 observer 字段代替（这样会有更强的功能和更好的性能）
+      }
+    },
+    myProperty2: String // 简化的定义方式
+  },
+  data: {}, // 私有数据，可用于模板渲染
+
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached() { },
+    moved() { },
+    detached() { },
+  },
+
+  // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+  attached() { }, // 此处attached的声明会被lifetimes字段中的声明覆盖
+  ready() { },
+
+  pageLifetimes: {
+    // 组件所在页面的生命周期函数
+    show() { },
+    hide() { },
+    resize() { },
+  },
+
+  methods: {
+
+  }
+})
